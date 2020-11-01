1861cb3b275af62bc5d02a6c84be0438a7bd5f1e
harrypotterse <mr.bean.mg22@gmail.com>
2020-11-01 10:54:33 +0200

    e6

--- .history/backend/user/item/create.blade_20201101105424.php
+++ .history/backend/user/item/create.blade_20201101105424.php
@@ -851,7 +851,7 @@
             "    </span>\n" +
             "    @enderror\n" +
             "</div>\n" +
-            "<div class=\"col-md-6\">\n" +
+            "<div class=\"col-md-5\">\n" +
             "    <label for=\"city_id\" class=\"text-black\">{{ __('backend.city.city') }}</label>\n" +
             "    <select id=\"select_city_id_\" class=\"custom-select @error('city_id') is-invalid @enderror\" name=\"city_id\">\n" +
             "        <option selected>{{ __('backend.item.select-city') }}</option>\n" +
