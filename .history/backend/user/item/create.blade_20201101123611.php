e0bfa7f624ef031859b2d44224ff7194fa8821cd
harrypotterse <mr.bean.mg22@gmail.com>
2020-11-01 12:38:56 +0200

    e6

--- .history/backend/user/item/create.blade_20201101123611.php
+++ .history/backend/user/item/create.blade_20201101123611.php
@@ -840,8 +840,8 @@
         c++;
        // e.preventDefault();
          $("#add").append("<div class=\"col-md-3\">\n" +
-            "    <label for=\"state_id\" for=\"state_id\" class=\"text-black\">{{ __('backend.state.state') }}</label>\n" +
-            "    <select id=\"${c} \" class=\" select_state_id_ custom-select @error('state_id') is-invalid @enderror\" name=\"state_id\">\n" +
+            "    <label for=\"state_id\" class=\"text-black\">{{ __('backend.state.state') }}</label>\n" +
+            "    <select id=\"select_state_id_\" class=\" select_state_id_ custom-select @error('state_id') is-invalid @enderror\" name=\"state_id\">\n" +
             "        <option selected>{{ __('backend.item.select-state') }}</option>\n" +
             "        @foreach($all_states as $key => $state)\n" +
             "        <option value=\"{{ $state->id }}\" {{ $state->id == old('state_id') ? 'selected' : '' }}>\n" +
--- .history/backend/user/item/create.blade_20201101123655.php
+++ .history/backend/user/item/create.blade_20201101123655.php
@@ -841,7 +841,7 @@
        // e.preventDefault();
          $("#add").append("<div class=\"col-md-3\">\n" +
             "    <label for=\"state_id\" for=\"state_id\" class=\"text-black\">{{ __('backend.state.state') }}</label>\n" +
-            "    <select id=\"${c} \" class=\" select_state_id_ custom-select @error('state_id') is-invalid @enderror\" name=\"state_id\">\n" +
+            "    <select id=\"select_state_id_ \" .cclass=\" select_state_id_ custom-select @error('state_id') is-invalid @enderror\" name=\"state_id\">\n" +
             "        <option selected>{{ __('backend.item.select-state') }}</option>\n" +
             "        @foreach($all_states as $key => $state)\n" +
             "        <option value=\"{{ $state->id }}\" {{ $state->id == old('state_id') ? 'selected' : '' }}>\n" +
